c3ad3a41325fa9489e154849a5552e1ddf1ee961
Big-problem <117372204+Big-problem@users.noreply.github.com>
2024-02-05 14:17:45 +0800

    tutorial 3 ended

---
+++ tutorial3.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(): # Extend the html file from the base.html
+    return render_template("extend.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
